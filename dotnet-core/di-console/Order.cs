6f7ab7730964067accb28d1c6c7c1d910b4c9202
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2018-11-13 16:54:54 +0100

    Added azure function project

--- dotnet-core/di-console/Order.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace samples.dependencyInjection
-{
-    public class Order {        
-        public string Guid { get; set; }
-        public string OrderId { get; set; }
-    }
-}
